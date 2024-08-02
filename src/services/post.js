f0ff777cba1205bc1539e08e3a352e1438f58461
Your Name <you@example.com>
2024-08-02 15:25:58 +0700

    s5

--- src/services/post.js
+++ src/services/post.js
@@ -34,22 +34,24 @@ export const getPostsService = () =>
 
 export const getPostsLimitService = (
   page,
-  query,
+  { limitPost, order, ...query },
   { priceNumber, areaNumber }
 ) =>
   new Promise(async (relsove, reject) => {
     try {
       let offset = !page || +page <= 1 ? 0 : +page - 1;
       const queries = { ...query };
-      if (priceNumber) queries.priceNumber = { [Op.between]: priceNumber };
-      if (areaNumber) queries.areaNumber = { [Op.between]: areaNumber };
+      const limit = +limitPost || +process.env.LIMIT;
+      queries.limit = limit;
+      if (priceNumber) query.priceNumber = { [Op.between]: priceNumber };
+      if (areaNumber) query.areaNumber = { [Op.between]: areaNumber };
+      if (order) queries.order = [order];
       const response = await db.Post.findAndCountAll({
-        where: queries,
+        where: query,
         raw: true,
         nest: true,
-        offset: offset * +process.env.LIMIT,
-        limit: +process.env.LIMIT,
-        order: [["createdAt", "DESC"]],
+        offset: offset * limit,
+        ...queries,
         include: [
           { model: db.Image, as: "images", attributes: ["image"] },
           {
@@ -58,6 +60,7 @@ export const getPostsLimitService = (
             attributes: ["price", "acreage", "published", "hashtag"],
           },
           { model: db.User, as: "user", attributes: ["name", "zalo", "phone"] },
+          { model: db.Overview, as: "overviews",attributes:['area', 'bonus', 'code', 'created','expired', 'target','type'] },
         ],
         attributes: ["id", "title", "star", "address", "description"],
       });
@@ -230,47 +233,66 @@ export const getPostsLimitAdminService = (page, id, query) =>
     }
   });
 
-  export const updatePost = ({ postId, overviewId, imagesId, attributesId, ...body }) =>
+export const updatePost = ({
+  postId,
+  overviewId,
+  imagesId,
+  attributesId,
+  ...body
+}) =>
   new Promise(async (relsove, reject) => {
     try {
-      const labelCode= generateCode(body.label)
-      await db.Post.update({
-        title: body.title,
-        labelCode,
-        address: body.address || null,
-        categoryCode: body.categoryCode,
-        description: JSON.stringify(body.description) || null,
-        areaCode: body.areaCode || null,
-        priceCode: body.priceCode || null,
-        provinceCode: body?.province?.includes("Thành phố")
-          ? generateCode(body?.province?.replace("Thành phố", ""))
-          : generateCode(body?.province?.replace("Tỉnh", "")) || null,
-        priceNumber: body.priceNumber,
-        areaNumber: body.areaNumber,
-      },{
-        where:{id:postId}
-      });
-      await db.Attribute.update({
-        price:
-          +body.priceNumber < 1
-            ? `${+body.priceNumber * 1000000} đồng/tháng`
-            : `${body.priceNumber} triệu/tháng`,
-        acreage: `${body.areaNumber} m2`,
-      },{
-        where:{id: attributesId}
-      });
-      await db.Image.update({
-        image: JSON.stringify(body.images),
-      },{
-        where:{id: imagesId}
-      });
-      await db.Overview.update({
-        area: body.label,
-        type: body?.category,
-        target: body?.target,
-      },{
-        where:{id: overviewId}
-      });
+      const labelCode = generateCode(body.label);
+
+      await db.Post.update(
+        {
+          title: body.title,
+          labelCode,
+          address: body.address || null,
+          categoryCode: body.categoryCode,
+          description: JSON.stringify(body.description) || null,
+          areaCode: body.areaCode || null,
+          priceCode: body.priceCode || null,
+          provinceCode: body?.province?.includes("Thành phố")
+            ? generateCode(body?.province?.replace("Thành phố", ""))
+            : generateCode(body?.province?.replace("Tỉnh", "")) || null,
+          priceNumber: body.priceNumber,
+          areaNumber: body.areaNumber,
+        },
+        {
+          where: { id: postId },
+        }
+      );
+      await db.Attribute.update(
+        {
+          price:
+            +body.priceNumber < 1
+              ? `${+body.priceNumber * 1000000} đồng/tháng`
+              : `${body.priceNumber} triệu/tháng`,
+          acreage: `${body.areaNumber} m2`,
+        },
+        {
+          where: { id: attributesId },
+        }
+      );
+      await db.Image.update(
+        {
+          image: JSON.stringify(body.images),
+        },
+        {
+          where: { id: imagesId },
+        }
+      );
+      await db.Overview.update(
+        {
+          area: body.label,
+          type: body?.category,
+          target: body?.target,
+        },
+        {
+          where: { id: overviewId },
+        }
+      );
       await db.Province.findOrCreate({
         where: {
           [Op.or]: [
@@ -293,15 +315,30 @@ export const getPostsLimitAdminService = (page, id, query) =>
         },
         defaults: {
           code: labelCode,
-          value: body?.label,
+          value: body.label,
         },
       });
+
       relsove({
         err: 0,
-        msg: "Updated" ,
+        msg: "Updated",
       });
     } catch (error) {
       reject(error);
     }
   });
 
+export const deletePost = (postId) =>
+  new Promise(async (relsove, reject) => {
+    try {
+      const response = await db.Post.destroy({
+        where: { id: postId },
+      });
+      relsove({
+        err: response > 0 ? 0 : 1,
+        msg: response > 0 ? "Delete" : "No post delete.",
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
--- src/services/user.js
+++ src/services/user.js
@@ -5,15 +5,31 @@ export const getOne = (id) =>
   new Promise(async (resolve, reject) => {
     try {
       const response = await db.User.findOne({
-        where:{id},
+        where: { id },
         raw: true,
         attributes: {
-            exclude:['password']
+          exclude: ["password"],
         },
       });
       resolve({
         err: response ? 0 : 1,
         msg: response ? "OK" : "Failed to get user",
+        response,
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+
+export const updateUser = (payload, id) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const response = await db.User.update(payload, {
+        where: { id },
+      });
+      resolve({
+        err: response[0] > 0 ? 0 : 1,
+        msg: response[0] > 0 ? "Updated" : "Failed to update user",
         response,
       });
     } catch (error) {
